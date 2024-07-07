f115f62ed18913df1b2d6a3d7fd43f285f33e4cc
archibate <1931127624@qq.com>
2024-07-08 01:52:21 +0800

    upgrade some functional

--- e1.cpp
+++ e1.cpp
@@ -1,5 +1,11 @@
-#include <print>
+#include <fmt/format.h>
+
+int hello(int x, int y) {
+    fmt::println("hello({}, {})", x, y);
+    return x + y;
+}
 
 int main() {
-    std::println("Hello, World!");
+    fmt::println("main 调用 hello 结果：{}", hello(2, 3));
+    return 0;
 }
