39d510204ac278ba21295d6296011d41800b0488
archibate <1931127624@qq.com>
2024-07-17 19:22:32 +0800

    fixindex

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ def define_env(env):
         warn='<img src="../img/warning.png" height="30px" width="auto" style="margin: 0; border: none"/>',
         detail='<img src="../img/question.png" height="30px" width="auto" style="margin: 0; border: none"/>',
     )
+    env.variables.icon2 = dict(
+        fun='<img src="./img/awesomeface.png" height="30px" width="auto" style="margin: 0; border: none"/>',
+        tip='<img src="./img/bulb.png" height="30px" width="auto" style="margin: 0; border: none"/>',
+        story='<img src="./img/book.png" height="30px" width="auto" style="margin: 0; border: none"/>',
+        warn='<img src="./img/warning.png" height="30px" width="auto" style="margin: 0; border: none"/>',
+        detail='<img src="./img/question.png" height="30px" width="auto" style="margin: 0; border: none"/>',
+    )
